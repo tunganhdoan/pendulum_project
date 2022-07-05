ddf0031b7c9e11991d87b219966702de2f221936
DOANTUNGANH <tunganh.doan@lqdtu.edu.vn>
2022-07-05 16:59:05 +0700

    create length inputs and declare DoubleVar()

--- main_frame.py
+++ main_frame.py
@@ -1,11 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
 
+
 class MainFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
 
+        def length_slider_changed(event):
+            pass
         options = {'padx': 5, 'pady': 5}
+        length = tk.DoubleVar(value=1.0)
+        length.set(1.0)
         length_label = ttk.Label(self, text="Length").grid(column=0, row=0, sticky=tk.E, **options)
-
-        self.grid(padx=5, pady=5, sticky=tk.NSEW)
+        length_scale = ttk.Scale(self, variable=length, command=length_slider_changed, orient='horizontal', length=150, from_=1, to=50).grid(column=1, row=0, sticky=tk.E, **options)
+        length_spinbox = ttk.Spinbox(self, textvariable=length, wrap=True,width=15, from_=1, to=50).grid(column=2, row=0, sticky=tk.E, **options)
+        self.grid(padx=50, pady=50, sticky=tk.NSEW)
