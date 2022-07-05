a603ad5a36e37c5694fc1f57fa162cc19ed44d56
DOANTUNGANH <tunganh.doan@lqdtu.edu.vn>
2022-07-05 16:36:21 +0700

    create length_label in main_frame.py

--- main_frame.py
+++ main_frame.py
@@ -4,3 +4,8 @@ from tkinter import ttk
 class MainFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
+
+        options = {'padx': 5, 'pady': 5}
+        length_label = ttk.Label(self, text="Length").grid(column=0, row=0, sticky=tk.E, **options)
+
+        self.grid(padx=5, pady=5, sticky=tk.NSEW)
