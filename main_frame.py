252a5ff4f342113b8b2d734256b641e722dab239
DOANTUNGANH <tunganh.doan@lqdtu.edu.vn>
2022-07-05 23:54:03 +0700

    add mass input widgets

--- main_frame.py
+++ main_frame.py
@@ -15,13 +15,21 @@ class MainFrame(ttk.Frame):
             length.set(round(length.get(), 1))
             renew_calculation()
 
+        def mass_slider_changed(event):
+            mass.set(round(mass.get(), 1))
+            renew_calculation()
+
+        def mass_spinbox_changed():
+            mass.set(round(mass.get(), 1))
+            renew_calculation()
+
         def renew_calculation():
             pass
 
         # get length of pendulum
         length = tk.DoubleVar(value=1.0)
 
-        ttk.Label(self, text="Length") \
+        ttk.Label(self, text="Length (m)") \
             .grid(column=0, row=0, sticky=tk.E, **options)
         ttk.Scale(self, variable=length, command=length_slider_changed,
                   orient='horizontal', length=150, from_=1, to=50) \
@@ -29,4 +37,18 @@ class MainFrame(ttk.Frame):
         ttk.Spinbox(self, textvariable=length, command=length_spinbox_changed,
                     increment=0.1, wrap=True, width=5, from_=1, to=50) \
             .grid(column=2, row=0, sticky=tk.E, **options)
+
+        # get mass of pendulum
+        mass = tk.DoubleVar(value=1.0)
+
+        ttk.Label(self, text="Mass (kg)") \
+            .grid(column=0, row=1, sticky=tk.E, **options)
+        ttk.Scale(self, variable=mass, command=mass_slider_changed,
+                  orient='horizontal', length=150, from_=1, to=50) \
+            .grid(column=1, row=1, sticky=tk.E, **options)
+        ttk.Spinbox(self, textvariable=mass, command=mass_spinbox_changed,
+                    increment=0.1, wrap=True, width=5, from_=1, to=50) \
+            .grid(column=2, row=1, sticky=tk.E, **options)
         self.grid(padx=50, pady=50, sticky=tk.NSEW)
+
+
