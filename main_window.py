819675b0f87e3c494ce164e6f17316e12134293e
DOANTUNGANH <tunganh.doan@lqdtu.edu.vn>
2022-07-05 22:50:34 +0700

    remove variable's widget for easy reading

--- main_window.py
+++ main_window.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.state('zoomed') # full-screen
+        self.state('zoomed')  # full-screen
         self.title("Pendulum Simulation (ver:1.0)")
         self.geometry('1360x768+300+20')
         self.iconbitmap('./assets/pd.ico')
